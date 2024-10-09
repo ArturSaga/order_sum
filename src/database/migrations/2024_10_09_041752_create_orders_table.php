8f8a5cc7e9ff648494940f270158a3f17bfdef5b
artursagataev <asagataev@findmykids.org>
2024-10-09 13:10:05 +0700

    fix migrate

--- src/database/migrations/2024_10_09_041752_create_orders_table.php
+++ src/database/migrations/2024_10_09_041752_create_orders_table.php
@@ -15,8 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->date('order_date');
-            $table->timestamps();
+            $table->timestamps('order_date');
         });
     }
 
